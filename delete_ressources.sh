f4332735fa8b9181d4586ed9f30e666e76c29e02
Steve Hiobi <steve.hiobi@gmail.com>
2020-04-07 09:56:07 +0200

    Updated delete_resources.sh

--- delete_ressources.sh
+++ delete_ressources.sh
@@ -1,26 +1,44 @@
 ##################################
 #Delete MySQL/MariaDb
 ##################################
+echo '##################################'
+echo 'Delete MySQL/MariaDb'
+echo '##################################'
 sudo systemctl stop mysql
 sudo apt-get -y remove --purge mysql-server mysql-client mysql-common
-sudo apt-get -y autoremove
-sudo apt-get -y autoclean
 sudo rm -rf /var/lib/mysql/
 sudo rm -rf /etc/mysql/
 
 ##################################
 #Delete PHP.7.3
 ##################################
+echo '##################################'
+echo 'Delete PHP.7.3'
+echo '##################################'
 sudo apt-get install -y ppa-purge
 sudo ppa-purge ppa:ondrej/php-7.3
 
 ##################################
 #Remove nginx
 ##################################
-sudo apt-get purge --auto-remove nginx
-sudo apt-get autoremove
+echo '##################################'
+echo 'Remove nginx'
+echo '##################################'
+sudo apt-get purge --auto-remove nginx -y
 
 ##################################
 #Remove nextcloud
 ##################################
-sudo rm -rf /var/www/nextcloud/
+echo '##################################'
+echo 'Remove nextcloud'
+echo '##################################'
+sudo rm -rf /var/www/nextcloud/ -y
+
+##################################
+#Cleanup unused packegas
+##################################
+echo '##################################'
+echo 'Cleanup unused packegas'
+echo '##################################'
+sudo apt-get -y autoremove
+sudo apt-get -y autoclean
